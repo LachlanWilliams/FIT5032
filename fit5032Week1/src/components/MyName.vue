9870b5e4e00b1a191ffe4c732c971e3f9cc85642
Lachlan Williams <lwil0039@student.monash.edu>
2024-07-23 14:02:15 +1000

    Week 1 MyName added

---
+++ fit5032Week1/src/components/MyName.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h1>{{ message }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MyName',
+    data() {
+      return {
+        message: 'Welcome, Lachlan Williams!'
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  
