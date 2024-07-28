f6abd2478bdb200fd6e1439162d656739c1ab6bf
Lachlan Williams <lwil0039@student.monash.edu>
2024-07-29 08:40:00 +1000

    week 2 activity 2

--- fit5032Week1/src/components/JSON.vue
+++ fit5032Week1/src/components/JSON.vue
@@ -98,7 +98,8 @@ const showMessage = ref(false)
 // Activity 2: Get authors born after 1850
 const modernAuthors = computed(() => {
   // TODO: CODE TO FILTER ARRAY OF AUTHORS HERE
-})
+  authors.filter((author) => author.birthYear > 1850)
+});
 
 // Activity 3: Get all famous works
 const allFamousWorks = computed(() => {
